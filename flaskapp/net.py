a6eb8bc36c30ca5a4949598aba5e55239ac4acb7
Zhobra <44748646+Zhobra@users.noreply.github.com>
2021-06-01 15:51:05 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -6,7 +6,7 @@ from keras.layers import Input
 from keras.models import Model
 # одна из предобученных сетей
 from keras.applications import MobileNetV2
-from keras.applications.mobilenet import preprocess_input, decode_predictions 
+from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions 
 import os
 # модуль работы с изображениями
 from PIL import Image
