fade702877a326bcdc732a7ce13c624ed9b7e469
Zhobra <44748646+Zhobra@users.noreply.github.com>
2021-06-01 23:40:52 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -27,7 +27,7 @@ ncol=3
 # загружаем и создаем стандартную уже обученную сеть keras
 visible2 = Input(shape=(nh,nw,ncol),name = 'imginp')
 resnet = MobileNetV2(include_top=True,
-weights=None, input_tensor=visible2,
+weights="imagenet", input_tensor=visible2,
 input_shape=None, pooling=None, classes=1000)
 # чтение изображений из каталога
 # учтите, если там есть файлы не соответствующие изображениям или каталоги
