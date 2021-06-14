906f3fa6c72b24fce0f62e334936b3465d8e8af9
Zhobra <44748646+Zhobra@users.noreply.github.com>
2021-06-14 19:58:37 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #декоратор для вывода страницы по умолчанию
 @app.route("/")
 def hello():
- return " <html><head></head> <body> Hello World! </body></html>"
+ return " <html><head></head> <body> Hello World! <br> We have <br> <a href="/net">Neural Net</a> </body></html>"
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
 from flask import render_template
